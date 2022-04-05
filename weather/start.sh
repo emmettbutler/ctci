c4300827ab8f9026e8f8a50c8b93fab719712cde
Emmett Butler <emmett.butler321@gmail.com>
2022-04-04 17:31:50 -0700

    add test option to makefile

---
+++ weather/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z "${ENVIRONMENT}" ]
+then
+    ENVIRONMENT="dev"
+fi
+
+
+run_test() {
+    py.test
+}
+
+run_local() {
+  python -u main.py
+}
+
+echo -ne "\n\n##\n##\tRUNNING WITH ENVIRONMENT=\"${ENVIRONMENT}\"\n##\n\n"
+if [ "${ENVIRONMENT}" == "test" ]
+then
+    run_test
+fi
+
+if [ "${ENVIRONMENT}" == "dev" ]
+then
+    run_local
+fi
