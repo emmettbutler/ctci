eba6d48fc101704f11c4bc914d113b9f1b989e24
Emmett Butler <emmett.butler321@gmail.com>
2022-04-15 07:16:46 -0700

    flesh out viewport tests

--- tests-demo/cypress/integration/sample_spec.js
+++ tests-demo/cypress/integration/sample_spec.js
@@ -13,16 +13,20 @@ describe('The RaptorMaps Public Site', () => {
         cy.url().should('eq', 'https://raptormaps.com/rmtechnology/')
     })
 
-    it('', () => {
+    it('The API section of the Technology page shows the expected elements', () => {
         cy.url().should('eq', 'https://raptormaps.com/rmtechnology/')
 
         var expected_text = "Raptor Maps easily integrates software solutions through our API. Clients can use the API to push or pull data between tools, maximizing the value of both tools while increasing data output and analysis. Our API can integrate with leading analytics tools used in asset monitoring dashboards and SCADA systems, including Power BI, SAP, and Power Factors.",
             expected_title = "API Integration",
             expected_img_src = "https://raptormaps.com/wp-content/uploads/2019/09/Raptor-Maps-API.png",
             expected_button_contents = "KNOWLEDGE HUB",
-            expected_button_target = "https://docs.raptormaps.com/reference/reference-getting-started#reference-getting-started";
+            expected_button_target = "https://docs.raptormaps.com/reference#reference-getting-started";
         cy.get('#tech-api').scrollIntoView()
 
         cy.get('h1').contains(expected_title).should('be.inViewport')
+        cy.get('p').contains(expected_text).should('be.inViewport')
+        cy.get('.et_pb_row_12').find('img').should('have.attr', 'src', expected_img_src).should('be.inViewport')
+        cy.get('a').contains(expected_button_contents).should('be.inViewport')
+            .should('have.attr', 'href', expected_button_target).should('be.inViewport')
     })
 })
