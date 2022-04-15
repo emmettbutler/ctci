0de1b398cd40c4320be1af8ab67bbeda77d58708
Emmett Butler <emmett.butler321@gmail.com>
2022-04-15 07:24:21 -0700

    button click test

--- tests-demo/cypress/integration/sample_spec.js
+++ tests-demo/cypress/integration/sample_spec.js
@@ -4,6 +4,8 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
 
+var KNOWLEDGEGE_HUB_BUTTON_TEXT = "KNOWLEDGE HUB"
+
 describe('The RaptorMaps Public Site', () => {
     it('The Jobs page can navigate to the Technology page', () => {
         var technology_link_id = '#menu-item-6777'
@@ -19,14 +21,22 @@ describe('The RaptorMaps Public Site', () => {
         var expected_text = "Raptor Maps easily integrates software solutions through our API. Clients can use the API to push or pull data between tools, maximizing the value of both tools while increasing data output and analysis. Our API can integrate with leading analytics tools used in asset monitoring dashboards and SCADA systems, including Power BI, SAP, and Power Factors.",
             expected_title = "API Integration",
             expected_img_src = "https://raptormaps.com/wp-content/uploads/2019/09/Raptor-Maps-API.png",
-            expected_button_contents = "KNOWLEDGE HUB",
             expected_button_target = "https://docs.raptormaps.com/reference#reference-getting-started";
         cy.get('#tech-api').scrollIntoView()
 
         cy.get('h1').contains(expected_title).should('be.inViewport')
         cy.get('p').contains(expected_text).should('be.inViewport')
         cy.get('.et_pb_row_12').find('img').should('have.attr', 'src', expected_img_src).should('be.inViewport')
-        cy.get('a').contains(expected_button_contents).should('be.inViewport')
+        cy.get('a').contains(KNOWLEDGEGE_HUB_BUTTON_TEXT).should('be.inViewport')
             .should('have.attr', 'href', expected_button_target).should('be.inViewport')
+    })
+
+    it('The Knowledge Hub button should open the expected page in a new tab', () => {
+        cy.url().should('eq', 'https://raptormaps.com/rmtechnology/')
+        var button = cy.get('a').contains(KNOWLEDGEGE_HUB_BUTTON_TEXT)
+        button.should('have.attr', 'target', '_blank')
+        button.invoke('removeAttr', 'target')
+        button.click()
+        cy.url().should('eq', 'https://docs.raptormaps.com/reference/reference-getting-started#reference-getting-started')
     })
 })
