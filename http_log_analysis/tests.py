75cf4b8d5fe1390d29e96eeebaf21b5cc96e027f
Emmett Butler <emmett.butler321@gmail.com>
2022-08-19 10:59:37 -0700

    clean up tests

--- http_log_analysis/tests.py
+++ http_log_analysis/tests.py
@@ -1,8 +1,14 @@
 import unittest
 from unittest.mock import Mock
 
-from main import (AccessLogAggregate, AccessLogEvent, AccessLogMonitor,
-                  aggregate_stats, read_access_log, section_from_request)
+from main import (
+    AccessLogAggregate,
+    AccessLogEvent,
+    AccessLogMonitor,
+    aggregate_stats,
+    read_access_log,
+    section_from_request,
+)
 
 
 class HTTPLogAnalyzerUnitTests(unittest.TestCase):
@@ -11,23 +17,6 @@ class HTTPLogAnalyzerUnitTests(unittest.TestCase):
         assert (
             result == "/api"
         ), "section_from_request() should extract the correct section"
-
-    def test_aggregate_stats(self):
-        agg1 = Mock(spec=AccessLogAggregate)
-        agg1.bucket = 123456789
-        agg1.total_events = 1
-        agg1.top_sections = {}
-        agg2 = Mock(spec=AccessLogAggregate)
-        agg2.bucket = 123456589
-        agg2.total_events = 1
-        agg2.top_sections = {}
-        aggregates = (a for a in [agg1, agg2])
-        result = list(aggregate_stats(aggregates, 10))
-        assert (
-            len(result) == 2
-        ), "aggregate_stats should return the same generator it consumed"
-        assert result[0] == agg1
-        assert result[1] == agg2
 
     def test_read_access_log(self):
         result = list(read_access_log("readme_sample_csv.txt", 0))
@@ -48,6 +37,10 @@ class AccessLogMonitorUnitTests(unittest.TestCase):
         agg2.total_events = 1
         agg2.top_sections = {}
         self.events = [agg1, agg2]
+
+    def test_aggregate_stats(self):
+        result = list(aggregate_stats(self.events, 10))
+        assert result == self.events
 
     def test_monitor_alerts(self):
         monitor = AccessLogMonitor(self.events, 1, 0)
@@ -80,8 +73,12 @@ class AccessLogAggregateUnitTests(unittest.TestCase):
         )
         agg = AccessLogAggregate(0, 12345, event)
         agg.add(AccessLogAggregate(0, 12346, event))
-        assert agg.total_events == 2, "total events counting from AccessLogAggregate.add should be accurate"
-        assert agg.top_sections["/api"] == 2, "top section counting from AccessLogAggregate.add should be accurate"
+        assert (
+            agg.total_events == 2
+        ), "total events counting from AccessLogAggregate.add should be accurate"
+        assert (
+            agg.top_sections["/api"] == 2
+        ), "top section counting from AccessLogAggregate.add should be accurate"
 
 
 if __name__ == "__main__":
